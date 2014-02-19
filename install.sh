22f9a8b254f2574abb52618695080b1267512406
Wayne Witzel III <wayne@pieceofpy.com>
2014-02-18 23:17:30 -0500

    Updated install.sh, no more command-t

--- install.sh
+++ install.sh
@@ -5,8 +5,3 @@ rm ~/.ackrc
 ln -s ${PWD}/vimrc ~/.vimrc
 ln -s ${PWD}/ ~/.vim
 ln -s ${PWD}/ackrc ~/.ackrc
-
-cd ${PWD}/bundle/command-t
-cd ${PWD}/ruby/command-t
-ruby extconf.rb && make clean && make
-
